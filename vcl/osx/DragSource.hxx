79f52ec2af2341d1972047b9e89cc3d34c6ae5e0
Douglas Mencken <dougmencken@gmail.com>
2015-10-01 21:46:38 -0400

    use C++ exceptions for non-ObjC code only
    add SAL_THROW_IfNotObjectiveC macro to include/sal/types.h and use it
    
    C++ exception stuff isn't very compatible with Objective-C
    especially for GCC NeXT 32-bit ABI
    
    for Objective-C code there's @try { } @catch ( ) { }
    
    low-level parts changed ~
        include/com/sun/star/uno/genfunc.hxx
        include/com/sun/star/uno/Reference.hxx
        include/cppuhelper/compbase.hxx
        include/cppuhelper/compbase_ex.hxx
        include/cppuhelper/implbase.hxx
        include/cppuhelper/weakagg.hxx
        include/cppuhelper/weak.hxx
        include/comphelper/weak.hxx
        include/comphelper/uno3.hxx
        codemaker/source/cppumaker/cpputype.cxx
        cppuhelper/source/factory.cxx
    
    Change-Id: Ia6f9f88bb34f417056564b77f62bd5ed1edb9f98

--- vcl/osx/DragSource.hxx
+++ vcl/osx/DragSource.hxx
@@ -84,13 +84,13 @@ public:
 
   // XInitialization
   virtual void SAL_CALL initialize( const com::sun::star::uno::Sequence< com::sun::star::uno::Any >& aArguments )
-    throw(com::sun::star::uno::Exception, std::exception/*, com::sun::star::uno::RuntimeException*/) override;
+    SAL_THROW_IfNotObjectiveC( com::sun::star::uno::Exception, std::exception/*, com::sun::star::uno::RuntimeException*/ ) override;
 
   // XDragSource
-  virtual sal_Bool SAL_CALL isDragImageSupported(  ) throw(com::sun::star::uno::RuntimeException, std::exception) override;
+  virtual sal_Bool SAL_CALL isDragImageSupported(  ) SAL_THROW_IfNotObjectiveC( com::sun::star::uno::RuntimeException, std::exception ) override;
 
   virtual sal_Int32 SAL_CALL getDefaultCursor(sal_Int8 dragAction)
-    throw(com::sun::star::lang::IllegalArgumentException, com::sun::star::uno::RuntimeException, std::exception) override;
+    SAL_THROW_IfNotObjectiveC( com::sun::star::lang::IllegalArgumentException, com::sun::star::uno::RuntimeException, std::exception ) override;
 
   virtual void SAL_CALL startDrag( const com::sun::star::datatransfer::dnd::DragGestureEvent& trigger,
                                    sal_Int8 sourceActions,
@@ -98,12 +98,15 @@ public:
                                    sal_Int32 image,
                                    const com::sun::star::uno::Reference< com::sun::star::datatransfer::XTransferable >& transferable,
                                    const com::sun::star::uno::Reference< com::sun::star::datatransfer::dnd::XDragSourceListener >& listener )
-    throw(com::sun::star::uno::RuntimeException, std::exception) override;
+    SAL_THROW_IfNotObjectiveC( com::sun::star::uno::RuntimeException, std::exception ) override;
 
   // XServiceInfo
-  virtual OUString SAL_CALL getImplementationName() throw (com::sun::star::uno::RuntimeException, std::exception) override;
-  virtual sal_Bool SAL_CALL supportsService(const OUString& ServiceName) throw (com::sun::star::uno::RuntimeException, std::exception) override;
-  virtual com::sun::star::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() throw (com::sun::star::uno::RuntimeException, std::exception) override;
+  virtual OUString SAL_CALL getImplementationName()
+    SAL_THROW_IfNotObjectiveC( com::sun::star::uno::RuntimeException, std::exception ) override;
+  virtual sal_Bool SAL_CALL supportsService(const OUString& ServiceName)
+    SAL_THROW_IfNotObjectiveC( com::sun::star::uno::RuntimeException, std::exception ) override;
+  virtual com::sun::star::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames()
+    SAL_THROW_IfNotObjectiveC( com::sun::star::uno::RuntimeException, std::exception ) override;
 
   void saveMouseEvent(NSEvent* theEvent);
   unsigned int getSupportedDragOperations(bool isLocal) const;
